3b6bde013a588931756286868cc31086e359a888
AlencarJuh <juliana.762108@graduacao.fsa.br>
2026-05-11 09:40:31 -0300

    Add files via upload

---
+++ ACEX/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <form>
+        <label>CPF / CNPJ</label><br>
+        <input><br><br>
+
+        <label>Senha</label><br>
+        <input><br><br>
+
+        <label>Confirmar senha</label><br>
+        <input><br><br>
+
+        <button>Criar conta</button>
+    </form>
+</body>
+</html>
---
+++ ACEX/paciente.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <input placeholder="Pesquisar"><br>
+    
+    <h2>Ações</h2>
+    <a>Agendar consulta</a><br>
+    <a>Reagendar consulta</a><br>
+    <a>Cancelar consulta</a><br>
+    <a>Historico de consultas</a><br>
+
+    <h2>Próximas consultas</h2>
+    <a>Consulta</a>
+
+</body>
+</html>
